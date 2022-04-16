b0c42c8579aacb08eade32b5d550d5569506a746
martincusack979 <cusackmartin@gmail.com>
2022-04-16 18:19:50 +0100

    analysis1

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# Programming and Scripting Module Final Project
+
+# Author : Martin Cusack
+
+with open(r"C:\Users\cusac\OneDrive\Desktop\iris.csv" , 'r') as f:   # open iris data set CSV file as f
+    iris = f.read()
+    f.close ()
+
+import numpy as np
+
+import matplotlib as pyplot
+
