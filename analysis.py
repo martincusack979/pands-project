0d3e9a92e0beb4586ee7265ceb9a3a4628c36ed7
martincusack979 <cusackmartin@gmail.com>
2022-04-17 20:25:33 +0100

    iris data frame

--- analysis.py
+++ analysis.py
@@ -1,12 +1,14 @@
 # Programming and Scripting Module Final Project
 
-# Author : Martin Cusack
+# Author: Martin Cusack
 
-with open(r"C:\Users\cusac\OneDrive\Desktop\iris.csv" , 'r') as f:   # open iris data set CSV file as f
-    iris = f.read()
-    f.close ()
-
+# with open(r"C:\Users\cusac\OneDrive\Desktop\iris.csv" , 'r') as f:   # open Fisher's iris data set CSV file 
+    # iris = f.read()                             
+   #  f.close ()
+                                                                    
 import numpy as np
-
 import matplotlib as pyplot
+import pandas as pd
 
+iris = pd.read_csv(r"C:\Users\cusac\OneDrive\Desktop\iris.csv") # open Fisher's iris data set CSV file using pandas
+print(iris)
