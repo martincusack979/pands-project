f38eef686fbde862227de685409410fd4b9246da
martincusack979 <cusackmartin@gmail.com>
2022-04-18 19:31:21 +0100

    iris commit 2

--- analysis.py
+++ analysis.py
@@ -8,7 +8,10 @@
                                                                     
 import numpy as np
 import matplotlib as pyplot
-import pandas as pd
+import pandas as pd                                                 # use pandas to analyse data because it's in tabular form
 
 iris = pd.read_csv(r"C:\Users\cusac\OneDrive\Desktop\iris.csv") # open Fisher's iris data set CSV file using pandas
-print(iris)
+print(iris.head())                                              # view first five rows of data frame using .head() method
+print(iris.info())                                              # display names of columns and the data types they contain using .info() method
+print(iris.shape)                                               # display number of rows and columns using .shape
+print(iris.describe())                                          # display soem basic information on data set
