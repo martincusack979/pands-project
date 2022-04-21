98039fede6cd1fb580604ae4fe1bdc0112dca36e
martincusack979 <cusackmartin@gmail.com>
2022-04-21 20:02:23 +0100

    write to txt file

--- analysis.py
+++ analysis.py
@@ -4,13 +4,13 @@
                                                                     
 import numpy as np
 import matplotlib as pyplot
-import pandas as pd                                                 # use pandas to analyse data because Iris data set is in tabular format
+import pandas as pd                                                  # use pandas to analyse data because Iris data set is in tabular format
 
-iris = pd.read_csv(r"C:\Users\cusac\OneDrive\Desktop\iris.csv") # open Fisher's iris data set CSV file using pandas
-#print(iris.head())                                              # view first five rows of data frame using .head() method
-#print(iris.info())                                              # display names of columns and the data types they contain using .info() method
-#print(iris.shape)                                               # display number of rows and columns using .shape attribute
-#print(iris.describe())                                          # display some basic information on data set using .describe() method
+iris = pd.read_csv(r"C:\Users\cusac\OneDrive\Desktop\iris.csv")      # open Fisher's iris data set CSV file using pandas
+#print(iris.head())                                                  # view first five rows of data frame using .head() method
+#print(iris.info())                                                  # display names of columns and the data types they contain using .info() method
+#print(iris.shape)                                                   # display number of rows and columns using .shape attribute
+#print(iris.describe())                                              # display some basic information on data set using .describe() method
 
 SepalLengthCm_stats = (iris["SepalLengthCm"].mean(),                             # Generate summary statistics on SepalLength Cm column. Firstly, output the mean length with .mean()
 iris["SepalLengthCm"].median(),                                                  # Output the median length using .median()
@@ -26,6 +26,7 @@ iris["SepalWidthCm"].max())
 
 print(SepalWidthCm_stats)
 
+
 PetalLengthCm_stats = (iris["PetalLengthCm"].mean(),                             # Generate summary statistics on PetalLength Cm column, as above
 iris["PetalLengthCm"].median(),                                                   
 iris["PetalLengthCm"].min(),                                                      
@@ -39,3 +40,9 @@ iris["PetalWidthCm"].min(),
 iris["PetalWidthCm"].max())
 
 print(PetalWidthCm_stats)
+
+with open('summary.txt', 'w') as f:                                              # generate text file and write all summary statistics to it
+    f.write('Summary statistics: ' + str(SepalLengthCm_stats) + str (SepalWidthCm_stats) + str (PetalLengthCm_stats) +  str (PetalWidthCm_stats))
+
+
+
